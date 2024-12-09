6d0f812f36116d0f64a7b730ac240b30e07cf913
ioanacl-filip <filipioanaclaudia@yahoo.ro>
2024-12-09 15:49:33 +0200

    lab si tema 4

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Proiect_MP1.Data;
 using Proiect_MP1.Models;
+using Proiect_MP1.Models.ViewModels;
 
 namespace Proiect_MP1.Pages.Categories
 {
@@ -19,11 +21,28 @@ namespace Proiect_MP1.Pages.Categories
             _context = context;
         }
 
-        public IList<Category> Category { get;set; } = default!;
+        public IList<Category> Category { get; set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Category = await _context.Category.ToListAsync();
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+            .Include(c => c.EventCategories)
+              .ThenInclude(bc => bc.Eveniment)
+              .ThenInclude(b => b.EventPlanner)
+            .OrderBy(c => c.CategoryName)
+            .ToListAsync();
+
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                    .Where(c => c.ID == id.Value).Single();
+                CategoryData.Evenimente = category.EventCategories.Select(bc => bc.Eveniment);
+            }
         }
     }
 }
